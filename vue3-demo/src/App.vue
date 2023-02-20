03fdb4d30a234fbe8f2bdbdf11f1193ab6af3ea5
leo <wijayleo@163.com>
2023-02-20 17:30:58 +0800

    feat:vue demo优化1

--- vue3-demo/src/App.vue
+++ vue3-demo/src/App.vue
@@ -1,6 +1,6 @@
 <template>
 	<img alt="Vue logo" src="./assets/logo.png" />
-	<HelloWorld msg="Welcome to Your Vue.js111 + TypeScript App" />
+	<HelloWorld msg="Welcome to Your Vue.js111 22222+ TypeScript App" />
 </template>
 
 <script lang="ts">
